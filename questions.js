95831d7ee9c9d3d26a63bdecc5ada1cde3dfb658
mcastro24 <66285203+mcastro24@users.noreply.github.com>
2020-10-24 21:46:42 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "Is gambling illegal anywhere in the U.S?",
+      choices: ["California", "Hawaii", "Texas", "It is legal everywhere"],
+      answer: "Hawaii"
+    },
+    {
+      title: "Plural of the word Horse",
+      choices: ["Hoose", "Horsees", "Horses", "Hooses"],
+      answer: "Horses"
+    },
+    {
+      title: "Solve this riddle... First I have 4 legs, then I have 2, and before I pass away I have 3 legs, what am I?",
+      choices: ["A fish", "A big fish", "A human", "An extinct animal"],
+      answer: "A human"
+    },
+    {
+      title: "Why do people toss shoes into power lines?",
+      choices: ["Its to show victory", "A celebration", "No reason, it was just a trend", "It's for an urban myth"],
+      answer: "It's for an urban myth"
+    },
+    {
+      title: "What is Morale?",
+      choices: ["Hapiness", "Being able to surpass an obstacle", "The Cheerfulness of a group", "A food"],
+      answer: "The Cheerfulness of a group"
+    }
+  ];
+  
